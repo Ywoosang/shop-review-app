e7c0a0c51eb0570a177883aafa646b0de8a99a6b
Ywoosang <opellong13@gmail.com>
2024-05-23 20:37:41 +0900

    initial commit

---
+++ src/main/java/com/review/shop/ShopReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.review.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopReviewApiApplication.class, args);
+	}
+
+}
