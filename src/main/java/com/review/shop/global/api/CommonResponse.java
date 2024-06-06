046619f495e747f404ebb09d71de24deba9a3a06
Ywoosang <opellong13@gmail.com>
2024-06-06 23:23:29 +0900

    rename: 클래스명 충돌 해결을 위해 ApiResponse 에서 CommonResponse 로 변경

--- src/main/java/com/review/shop/global/api/CommonResponse.java
+++ src/main/java/com/review/shop/global/api/CommonResponse.java
@@ -3,44 +3,38 @@ package com.review.shop.global.api;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.review.shop.domain.user.dto.UserResponseDTO;
 import com.review.shop.global.api.code.BaseCode;
 import com.review.shop.global.api.code.status.SuccessStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
-public class ApiResponse<T> {
-    @JsonProperty("isSucess")
+public class CommonResponse<T> {
+    @JsonProperty("isSuccess")
     private final Boolean isSuccess;
 
     private final String code;
 
     private final String message;
+
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T result;
 
-    public static <T> ApiResponse<T> success(T result) {
-        return new ApiResponse<>(
+    public static <T> CommonResponse<T> success(T result) {
+        return new CommonResponse<T>(
                 true,
-                SuccessStatus._OK.getCode(),
-                SuccessStatus._OK.getMessage(),
+                SuccessStatus.OK.getCode(),
+                SuccessStatus.OK.getMessage(),
                 result
         );
     }
 
-    public static <T> ApiResponse<T> of(BaseCode code, T result) {
-        return new ApiResponse<>(
-                true,
-                code.getReasonHttpStatus().getCode(),
-                code.getReasonHttpStatus().getMessage(),
-                result
-        );
-    }
-
-    public static  <T> ApiResponse<T> failure(String code, String message, T result) {
-        return new ApiResponse<>(
+    public static  <T> CommonResponse<T> failure(String code, String message, T result) {
+        return new CommonResponse<>(
                 false,
                 code,
                 message,
