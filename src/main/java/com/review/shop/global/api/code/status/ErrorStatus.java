09508bf2ac59f0608c3b4e028b22a6b01aa8bbfd
Ywoosang <opellong13@gmail.com>
2024-06-06 23:12:24 +0900

    feat: 커스텀 에러코드 표기방식 변경 closes #8

--- src/main/java/com/review/shop/global/api/code/status/ErrorStatus.java
+++ src/main/java/com/review/shop/global/api/code/status/ErrorStatus.java
@@ -10,28 +10,31 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
 
-    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
-    _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON400","잘못된 요청입니다."),
-    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","인증이 필요합니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+    // Common Errors
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500_01", "관리자에게 문의 바랍니다."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_400_01", "잘못된 요청입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON_401_01", "인증이 필요합니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON_403_01", "금지된 요청입니다."),
 
+    // User Errors
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_404_01", "존재하지 않는 사용자입니다."),
+    FOOD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_404_02", "존재하지 않는 음식 카테고리입니다."),
+    USER_EMAIL_ALREADY_EXIST(HttpStatus.CONFLICT, "USER_409_01", "이미 사용중인 이메일입니다."),
 
-    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "사용자가 없습니다."),
-    NICKNAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "닉네임은 필수 입니다."),
+    // Store Errors
+    STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE_404_01", "존재하지 않는 가게입니다."),
+    STORE_REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE_404_O2", "존재하지 않는 지역입니다."),
 
+    // Article Errors
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE_404_01", "존재하지 않는 게시글입니다."),
 
-    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다."),
+    // Review Errors
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "REVIEW_404_01", "존재하지 않는 리뷰입니다."),
 
+    // Mission Errors
+    MISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "MISSION_404_01", "존재하지 않는 미션입니다.");
 
-    // FoodCategory Error
-    FOOD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "FOOD_CATEGORY4001", "음식 카테고리가 없습니다."),
-
-    // Store Error
-    STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE4001","가게가 없습니다."),
-
-    // Test
-    TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "테스트 응답");
-
+    // Test Error
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
