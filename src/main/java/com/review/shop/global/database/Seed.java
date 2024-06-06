25c1acea6e565f47d31af570a2992c82c1d5c8f6
Ywoosang <opellong13@gmail.com>
2024-06-06 23:10:28 +0900

    chore(seed): 테스트 데이터 삽입 설정

---
+++ src/main/java/com/review/shop/global/database/Seed.java
@@ -0,0 +1,66 @@
+package com.review.shop.global.database;
+
+import com.review.shop.domain.store.model.Region;
+import com.review.shop.domain.store.repository.RegionRepository;
+import com.review.shop.domain.user.model.FoodCategory;
+import com.review.shop.domain.user.repository.FoodCategoryRepository;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+public class Seed {
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.initFoodCategories();
+        initService.initRegions();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+        private final FoodCategoryRepository foodCategoryRepository;
+        private final RegionRepository regionRepository;
+
+        public void initFoodCategories() {
+            final String[] foodCategoryNames = {
+                    "한식", "일식", "중식",
+                    "양식","치킨","분식",
+                    "고기/구이", "도시락", "야식(족발,보쌈)",
+                    "패스트푸드", "디저트", "아시안푸드"
+            };
+
+            Arrays.stream(foodCategoryNames).forEach(name -> {
+                FoodCategory foodCategory = FoodCategory.builder()
+                        .name(name)
+                        .build();
+                foodCategoryRepository.save(foodCategory);
+            });
+        }
+
+        public void initRegions() {
+            final String[] regionNames = {
+                    "고덕1동","고덕2동","상일동","강일동",
+                    "명일1동","명일2동","길동",
+                    "천호1동","천호2동","천호3동",
+                    "성내1동","성내2동","성내3동",
+                    "둔촌1동","둔촌2동","암사1동","암사2동","암사3동"
+            };
+
+            Arrays.stream(regionNames).forEach(name -> {
+                Region region = Region.builder()
+                        .name(name)
+                        .build();
+                regionRepository.create(region);
+            });
+        }
+    }
+}
