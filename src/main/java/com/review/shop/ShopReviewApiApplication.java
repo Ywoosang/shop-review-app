0431abc2728904cdc09d1fa7860851bb1f3d289e
Ywoosang <opellong13@gmail.com>
2024-06-06 23:08:09 +0900

    feat: 생성, 수정 감지를 위한 JPA Auditing 추가

--- src/main/java/com/review/shop/ShopReviewApiApplication.java
+++ src/main/java/com/review/shop/ShopReviewApiApplication.java
@@ -2,12 +2,13 @@ package com.review.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ShopReviewApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopReviewApiApplication.class, args);
 	}
-
 }
--- src/main/java/com/review/shop/global/model/BaseEntity.java
+++ src/main/java/com/review/shop/global/model/BaseEntity.java
@@ -1,14 +1,17 @@
 package com.review.shop.global.model;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@MappedSuperclass
 @Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @CreatedDate
     private LocalDateTime createdAt;
