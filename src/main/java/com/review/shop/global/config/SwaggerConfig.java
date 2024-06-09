0f992530c9ee5294e8f97c6fbf671fe4cd00bf3b
Ywoosang <opellong13@gmail.com>
2024-06-10 01:57:47 +0900

    feat: api 요청에 jwt 토큰을 포함하도록 swagger 설정 업데이트

--- src/main/java/com/review/shop/global/config/SwaggerConfig.java
+++ src/main/java/com/review/shop/global/config/SwaggerConfig.java
@@ -3,15 +3,23 @@ package com.review.shop.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "JWT";
+
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
-                .info(apiInfo());
+                .info(apiInfo())
+                .addSecurityItem(apiSecurityRequirement())
+                .components(apiComponents());
+
     }
 
     private Info apiInfo() {
@@ -19,5 +27,18 @@ public class SwaggerConfig {
                 .title("API DOCS")
                 .description("Shop Review APP Swagger UI")
                 .version("1.0.0");
+    }
+
+    private SecurityRequirement apiSecurityRequirement() {
+        return new SecurityRequirement().addList(SECURITY_SCHEME_NAME);
+    }
+
+    private Components apiComponents() {
+        return new Components().addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                .name(SECURITY_SCHEME_NAME)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
     }
 }
