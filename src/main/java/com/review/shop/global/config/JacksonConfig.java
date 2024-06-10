3c174963ff0dd3d64df0b8c03b403eb17def0226
Ywoosang <opellong13@gmail.com>
2024-06-10 15:30:34 +0900

    feat: LocalDateTime 변환을 위한 ObjectMapper 설정 및 jsr310 라이브러리 추가

---
+++ src/main/java/com/review/shop/global/config/JacksonConfig.java
@@ -0,0 +1,21 @@
+package com.review.shop.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        // 배열이 아닌 ISO 8601 형식의 문자열로 직렬화되도록 설정한다.
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
+        return objectMapper;
+    }
+}
