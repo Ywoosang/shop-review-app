c316d65ef33b065e178de8b83c514029a532f321
Ywoosang <opellong13@gmail.com>
2024-06-11 15:44:45 +0900

    refactor: 리포지토리 인터페이스 정의 closes #14

--- src/main/java/com/review/shop/domain/user/repository/UserRepository.java
+++ src/main/java/com/review/shop/domain/user/repository/UserRepository.java
@@ -1,39 +1,12 @@
 package com.review.shop.domain.user.repository;
 
 import com.review.shop.domain.user.model.User;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.TypedQuery;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+
 import java.util.Optional;
 
 @Repository
-@RequiredArgsConstructor
-public class UserRepository {
-    private final EntityManager em;
-
-    public User save(User user) {
-        em.persist(user);
-        return user;
-    }
-
-    public Optional<User> findById(Long id) {
-        User user = em.find(User.class, id);
-        return Optional.ofNullable(user);
-    }
-
-    public List<User> findAll() {
-        return em.createQuery("select u from User u", User.class).getResultList();
-    }
-
-    public Optional<User> findByEmail(String email) {
-        TypedQuery<User> query = em.createQuery("select u from User u where u.email = :email", User.class);
-        query.setParameter("email", email);
-        List<User> results = query.getResultList();
-        if (results.isEmpty()) {
-            return Optional.empty();
-        }
-        return Optional.of(results.get(0));
-    }
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
 }
